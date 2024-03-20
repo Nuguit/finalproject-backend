7a73c407c4eea53d9ddd436ad831fb6d093d5ab8
Nuria Guevara <nuriaguevarafuentes@gmail.com>
2024-03-20 19:40:53 +0100

    1940 2003

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> {
     console.log ("Server listening on https://localhost:${PORT}");
